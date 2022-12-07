468042836b46c9b774aaa4dbc86a879533715967
maurocoimbra <90289072+maurocoimbra@users.noreply.github.com>
2022-12-07 18:41:22 -0300

    Print no terminal

--- Arvore/main.py
+++ Arvore/main.py
@@ -65,7 +65,7 @@ def on_buttonAdd_clicked():
 
         textDisplay = '\nÁrvore Binária de Pesquisa:\n'
         textDisplay += '\n'.join(arvore.display())
-        # print(textDisplay)
+        print(textDisplay)
         labelDisplay.setText(textDisplay)
 
 buttonAdd.clicked.connect(on_buttonAdd_clicked)
